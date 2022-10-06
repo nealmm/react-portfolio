b3c413798f4451e5d1358e43032aa5cad95b86cc
Matt Neal <neal.matt.m@gmail.com>
2022-10-05 23:54:22 -0400

    Setup project

---
+++ portfolio/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Matt Neal</h1>
+        <nav>
+          <a href="#about">About Me</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#contact">Contact</a>
+          <a href="#resume">Resume</a>
+        </nav>
+      </header>
+      <main>
+        <section id="about">
+          <h2>About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim facilisis gravida neque convallis a cras semper auctor neque. Purus sit amet luctus venenatis lectus magna. Neque viverra justo nec ultrices dui sapien eget mi proin. Massa tempor nec feugiat nisl pretium fusce. Sem integer vitae justo eget magna fermentum iaculis. Ultrices tincidunt arcu non sodales neque sodales ut. Massa massa ultricies mi quis. Neque convallis a cras semper. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Pellentesque habitant morbi tristique senectus et netus et. Enim praesent elementum facilisis leo vel fringilla. Lobortis feugiat vivamus at augue. Vitae purus faucibus ornare suspendisse sed. Et netus et malesuada fames ac turpis egestas.
+          </p>
+        </section>
+        <section id="portfolio">
+          <h2>Portfolio</h2>
+          <p>
+            Quis viverra nibh cras pulvinar mattis nunc sed. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Viverra vitae congue eu consequat ac felis. Sapien pellentesque habitant morbi tristique senectus et. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Vivamus arcu felis bibendum ut tristique et. Ultrices neque ornare aenean euismod. Volutpat maecenas volutpat blandit aliquam etiam erat velit. A diam maecenas sed enim ut sem. Risus at ultrices mi tempus imperdiet nulla.
+          </p>
+        </section>
+        <section id="contact">
+          <h2>Contact</h2>
+          <p>
+            Integer eget aliquet nibh praesent tristique magna sit. Nulla porttitor massa id neque aliquam vestibulum morbi blandit. Pharetra sit amet aliquam id. Massa enim nec dui nunc mattis enim ut tellus. Nunc vel risus commodo viverra maecenas accumsan lacus vel. Sodales ut eu sem integer vitae justo eget. Non nisi est sit amet. Sit amet consectetur adipiscing elit duis tristique. Aliquet nibh praesent tristique magna sit amet purus gravida. Leo urna molestie at elementum eu facilisis sed. Bibendum neque egestas congue quisque egestas diam in arcu. Scelerisque purus semper eget duis at tellus. Aliquam id diam maecenas ultricies mi. Nunc sed blandit libero volutpat sed cras ornare arcu dui.
+          </p>
+        </section>
+        <section id="resume">
+          <h2>Resume</h2>
+          <p>
+            Pharetra magna ac placerat vestibulum. Sit amet venenatis urna cursus eget. Lectus sit amet est placerat in egestas. Arcu risus quis varius quam quisque id. Consectetur a erat nam at lectus urna duis convallis convallis. Viverra aliquet eget sit amet tellus cras. Elit at imperdiet dui accumsan sit amet. Nunc mattis enim ut tellus elementum. A erat nam at lectus urna duis convallis. Luctus venenatis lectus magna fringilla. Tristique senectus et netus et malesuada fames ac turpis egestas. Ut faucibus pulvinar elementum integer enim neque volutpat ac. Ut lectus arcu bibendum at varius. Vitae semper quis lectus nulla at volutpat diam. Aliquam purus sit amet luctus venenatis lectus magna fringilla. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Sed odio morbi quis commodo odio aenean sed adipiscing diam. Senectus et netus et malesuada fames ac turpis egestas.
+          </p>
+        </section>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
